33c13156d6d4f6fbb7ce90c680d8cd088004b8f9
M7mdMuk <mohd98.mm@gmail.com>
2020-02-03 02:07:22 +0300

    Finished the design by adding 'our projects, contact us and the footer' and did the mobile screens.

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,19 @@
 // smooth scroll
-var scroll = new SmoothScroll('.nav-bar a[href*="#"]', {
+var scroll = new SmoothScroll('.nav-bar, a[href*="#"]', {
   speed: 700
 });
+
+var scroll = new SmoothScroll('#mySidenav, a[href*="#"]', {
+  speed: 700
+});
+
+// side navBar
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("side-nav-bar").style.display = "none";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("side-nav-bar").style.display = "block";
+}
