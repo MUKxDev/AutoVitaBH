99729cfe9a41dc4d87720ec77024d5ea9d432db5
M7mdMuk <mohd98.mm@gmail.com>
2020-02-06 16:55:54 +0300

    Added the E-mail sending functionality

--- public/JS/emails.js
+++ public/JS/emails.js
@@ -1,9 +1,56 @@
-var db = firebase.firestore();
+//firestore database ref
+var db = firebase.database();
 
-var emailsRef = db.collection("emails");
+//emails doc ref
+var emailsRef = db.ref("emails");
 
-emailsRef.get().then(snapshot => {
-  snapshot.docs.forEach(doc => {
-    console.log(doc.data());
+// emailsRef.get().then(snapshot => {
+//   snapshot.docs.forEach(doc => {
+//     console.log(doc.data());
+//   });
+// });
+
+// code
+
+("use strict");
+
+//grab a form
+const form = document.querySelector("#contact-form");
+
+//grab an input
+const name = form.querySelector("#name");
+const mailFrom = form.querySelector("#mail");
+const subject = form.querySelector("#subject");
+const message = form.querySelector("#message");
+
+//create a functions to push
+function submitMail() {
+  if (
+    name.value == "" ||
+    mailFrom.value == "" ||
+    subject.value == "" ||
+    message.value == ""
+  ) {
+    alert("Please fill all the fields");
+  } else {
+    emailsRef.push().set({
+      name: name.value,
+      mailFrom: mailFrom.value,
+      subject: subject.value,
+      message: message.value
+    });
+
+    // alert("Mail sent, Thank you.");
+  }
+}
+
+//push on form submit
+if (form) {
+  form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+    submitMail();
+
+    //shows alert if everything went well.
+    return alert("Data Successfully Sent to Realtime Database");
   });
-});
+}
